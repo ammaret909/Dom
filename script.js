ffc2c4776d067a84eafaa2ab2c1732ee555180b2
ammaret909 <ammaret_aituphon@cmu.ac.th>
2022-05-18 15:30:01 +0700

    finish 18

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-var newText = 'new Text'
-document.getElementById('ok').innerHTML = newText
 
-var kaw = 'Kaw'
-document.getElementById('no').innerHTML = "<b><i>"+kaw+"</i></b>"
+// document.getElementById('ok').addEventListener('click',function(e){
+//     alert('ok is clicked')
+//     console.log(e)
+// })
+
+document.getElementById('cancel').addEventListener('mousemove',function(e){
+    console.log('x =',e.screenX,'y =',e.screenY)
+})
+
+let elems = document.getElementsByClassName('flex-item')
+console.log(elems)
+for (let elem of elems){
+    elem.addEventListener('mousemove',function(e){
+        console.log("x =",e.screenX,"y =",e.screenY)
+    })
+    elem.addEventListener('click',function(e){
+        alert(elem.innerText)
+    })
+}
